340a86fcb8df06f316605e75f163a08ba40d7c41
mfergoda <mrfergoda@gmail.com>
2020-03-06 21:06:04 -0800

    Created basic functionality, settings module.

--- alien_invasion.py
+++ alien_invasion.py
@@ -1,9 +1,43 @@
 #!/usr/bin/env python3
 # -*- coding: utf-8 -*-
 """
-Created on Thu Mar  5 19:46:40 2020
+Alien Invasion! Game
+From Python Crash Course by Matthes
 
 @author: Matt
 """
+import sys
+import pygame
 
-print('hello, world!')
+from settings import Settings
+
+class AlienInvasion:
+    """Overall class to manage game assets and behavior."""
+    
+    def __init__(self):
+        """Initialize the game, and create game resources."""
+        pygame.init()
+        self.settings = Settings()
+        
+        self.screen = pygame.display.set_mode(
+            (self.settings.screen_width, self.settings.screen_height))
+        pygame.display.set_caption("Alien Invasion")
+        
+    def run_game(self):
+        """Start the main loop for the game."""
+        while True:
+            # Watch for keyboard and mouse events.
+            for event in pygame.event.get():
+                if event.type == pygame.QUIT:
+                    sys.exit()
+                    
+            # Redraw the screen during each pass through the loop.
+            self.screen.fill(self.settings.bg_color)
+            
+            # Make the most recently drawn screen visible
+            pygame.display.flip()
+            
+if __name__ == '__main__':
+    # MAke a game instance, and run the game
+    ai = AlienInvasion()
+    ai.run_game()
---
+++ settings.py
@@ -0,0 +1,18 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+"""
+Created on Fri Mar  6 20:47:03 2020
+Settings for alien_invasion.py
+
+@author: Matt
+"""
+
+class Settings:
+    """A class to store all settings for Alien Invasion"""
+    
+    def __init__(self):
+        """Initialize the game's settings"""
+        # Screen settings
+        self.screen_width = 1200
+        self.screen_height = 800
+        self.bg_color = (230, 230, 230)
