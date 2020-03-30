eb5bab71d943a64f8884a7eaa0cfb8ebbeffb326
mfergoda <mrfergoda@gmail.com>
2020-03-29 19:19:10 -0700

    Can now handle alien-ship collisions and aliens reaching the bottom of the screen. For now the game freezes after ships_left reaches zero. Added game_stats module.

--- settings.py
+++ settings.py
@@ -20,9 +20,10 @@ class Settings:
 
         # Ship settings
         self.ship_speed = 5
+        self.ship_limit = 3
 
         # Bullet settings
-        self.bullet_speed = 1.5
+        self.bullet_speed = 4
         self.bullet_width = 3
         self.bullet_height = 15
         self.bullet_color = (250,0,0)
@@ -30,6 +31,6 @@ class Settings:
         
         # Alien settings
         self.alien_speed = 1.0
-        self.fleet_drop_speed = 10
+        self.fleet_drop_speed = 100
         # fleet_direction of 1 represents right; -1 represents left.
         self.fleet_direction = 1
